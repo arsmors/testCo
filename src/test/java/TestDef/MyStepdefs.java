627c73a542fa60c492f7ec6b51ed9b583ad132a2
Arsens <arsens.morins@chi.lv>
2019-04-24 20:04:51 +0300

    keyw

--- src/test/java/TestDef/MyStepdefs.java
+++ src/test/java/TestDef/MyStepdefs.java
@@ -1,19 +1,32 @@
 package TestDef;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class MyStepdefs {
     WebDriver driver;
-
+//    public By search = By.id("search");
+    public By search = By.xpath("//input[@placeholder='Search…']");
 
     @When("^I get to stackoverflow webpage$")
     public void iGetToStackoverflowWebpage() throws Throwable {
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://stackoverflow.com");
+    }
+
+    @And("^In search field enter \"([^\"]*)\"$")
+    public void inSearchFieldEnter(String string) {
+        driver.findElement(search).click();
+        driver.findElement(search).sendKeys(string);
+        driver.findElement(search).submit();
+
     }
 }
--- target/test-classes/test.feature
+++ target/test-classes/test.feature
@@ -3,12 +3,12 @@
 
 Feature: A description
 
-  Scenario: A scenario
+  Scenario Outline: A scenario
     When I get to stackoverflow webpage
-#    And In search field enter "<keywords>"
+    And In search field enter "<keywords>"
 #    Then All relevant topics are saved in the textfile
-#    Examples:
-#      | keywords |
-#      | 2013     |
+    Examples:
+      | keywords |
+      | 2013     |
 
 
